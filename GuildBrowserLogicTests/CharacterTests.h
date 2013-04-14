ab619fd1f4dceb72f73df9e84c939d4f02053b03
yang junfeng <yangjunfeng@yangmatoMacBook-Pro.local>
2013-04-14 21:41:08 +0800

    add test target

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by yang junfeng on 13-4-14.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
